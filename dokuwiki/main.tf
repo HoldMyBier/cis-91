9a42238770391ea9e0bd7023a27e026aebf757ef
Elijah Morrison <morrisonejt@gmail.com>
2022-12-04 02:57:55 +0000

    name changes for service account

--- dokuwiki/main.tf
+++ dokuwiki/main.tf
@@ -56,15 +56,15 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
-resource "google_service_account" "lab08-service-account" {
-  account_id   = "lab08-service-account"
-  display_name = "lab08-service-account"
-  description = "Service account for lab 08"
+resource "google_service_account" "dokuwiki-service-account" {
+  account_id   = "dokuwiki-service-account"
+  display_name = "dokuwiki-service-account"
+  description = "Service account for dokuwiki"
 }
 
 resource "google_project_iam_member" "project_member" {
   role = "roles/compute.viewer"
-  member = "serviceAccount:${google_service_account.lab08-service-account.email}"
+  member = "serviceAccount:${google_service_account.dokuwiki-service-account}"
 }
 
 
